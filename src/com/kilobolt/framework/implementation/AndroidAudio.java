1c66dade8fdea009c1470a9667d9b773dc4cf31f
antoniotari <antonio.tari@digiflare.com>
2014-08-05 16:08:13 -0400

    indentation

--- src/com/kilobolt/framework/implementation/AndroidAudio.java
+++ src/com/kilobolt/framework/implementation/AndroidAudio.java
@@ -12,28 +12,35 @@ import com.kilobolt.framework.Audio;
 import com.kilobolt.framework.Music;
 import com.kilobolt.framework.Sound;
 
-public class AndroidAudio implements Audio {
+public class AndroidAudio implements Audio 
+{
     AssetManager assets;
     SoundPool soundPool;
 
-    public AndroidAudio(Activity activity) {
+    public AndroidAudio(Activity activity) 
+    {
         activity.setVolumeControlStream(AudioManager.STREAM_MUSIC);
         this.assets = activity.getAssets();
         this.soundPool = new SoundPool(20, AudioManager.STREAM_MUSIC, 0);
     }
 
     @Override
-    public Music createMusic(String filename) {
-        try {
+    public Music createMusic(String filename) 
+    {
+        try 
+        {
             AssetFileDescriptor assetDescriptor = assets.openFd(filename);
             return new AndroidMusic(assetDescriptor);
-        } catch (IOException e) {
+        } 
+        catch (IOException e) 
+        {
             throw new RuntimeException("Couldn't load music '" + filename + "'");
         }
     }
 
     @Override
-    public Sound createSound(String filename) {
+    public Sound createSound(String filename) 
+    {
         try {
             AssetFileDescriptor assetDescriptor = assets.openFd(filename);
             int soundId = soundPool.load(assetDescriptor, 0);
